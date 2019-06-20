400c3746650f7d2d8d5a46ca736a1defef99590b
One Punch <bodnguye@gmail.com>
2019-06-19 23:15:22 -0700

    orm updateOne

--- config/orm.js
+++ config/orm.js
@@ -4,7 +4,14 @@ var connection = require("../config/connection.js");
 var orm = {
     selectAll: function(tableInput, cb) {
         connection.query('SELECT * FROM ' + tableInput + ';', function(err, result) {
-            if (err) throw err;
+            if(err) throw err;
+            cb(result)
+        })
+    },
+
+    updateOne: function(tableInput, condition, cb) {
+        connection.query('UPDATE ' + tableInput + ' SET devoured=true WHERE id='+condition+';', function(err, result) {
+            if(err) throw err;
             cb(result)
         })
     }
